1d02ce3793a2b3efc631569381df2475b6e1ed4f
Richard Gonsuron <rgonsuron@gmail.com>
2016-06-13 21:06:37 -0500

    ramp motors up to speed

--- devicetypes/smartthings/on-off-shield.src/on-off-shield.groovy
+++ devicetypes/smartthings/on-off-shield.src/on-off-shield.groovy
@@ -16,6 +16,7 @@ metadata {
 		capability "Actuator"
 		capability "Switch"
 		capability "Sensor"
+        capability "Garage Door Control"
 	}
 
 	// Simulator metadata
@@ -31,8 +32,12 @@ metadata {
 	// UI tile definitions
 	tiles {
 		standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true, canChangeBackground: true) {
-			state "on", label: '${name}', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#79b821"
-			state "off", label: '${name}', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
+			// state "on", label: '${name}', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#79b821"
+			// state "off", label: '${name}', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
+			state "on", label: 'opened', action: "switch.off", icon: "st.doors.garage.garage-open", backgroundColor: "#79b821"
+			state "off", label: 'closed', action: "switch.on", icon: "st.doors.garage.garage-closed", backgroundColor: "#ffffff"
+            state "opening", label: '${name}', icon: "st.doors.garage.garage-opening", backgroundColor: "#b9b821"
+            state "closing", label: '${name}', icon: "st.doors.garage.garage-closing", backgroundColor: "#b9b821"
 		}
 
 		main "switch"
@@ -43,7 +48,7 @@ metadata {
 // Parse incoming device messages to generate events
 def parse(String description) {
 	def value = zigbee.parse(description)?.text
-	def name = value in ["on","off"] ? "switch" : null
+	def name = value in ["on","off","opening","closing"] ? "switch" : null
 	def result = createEvent(name: name, value: value)
 	log.debug "Parse returned ${result?.descriptionText}"
 	return result
