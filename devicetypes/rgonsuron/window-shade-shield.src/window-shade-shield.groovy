28b0fd6d96f5464abe643009a089ca673a6811be
Richard Gonsuron <rgonsuron@gmail.com>
2016-07-04 16:03:07 -0500

    none

--- devicetypes/rgonsuron/window-shade-shield.src/window-shade-shield.groovy
+++ devicetypes/rgonsuron/window-shade-shield.src/window-shade-shield.groovy
@@ -1,64 +1,45 @@
-/*
-	zigbee window shade device handler
+/**
 */
 metadata {
 	definition (name: "Window Shade Shield", namespace: "rgonsuron", author: "rgonsuron") {
-		// capability "Actuator"
-        capability "Garage Door Control"
+		capability "Actuator"
 		capability "Switch"
-		// capability "Sensor"
-        // capability "Window Shade"
-        attribute "currentSwitch", "string"
 	}
 
-	// Simulator metadata
 	simulator {
-		status "open":  "catchall: 0104 0000 01 01 0040 00 0A21 00 00 0000 0A 00 0A6F6E"
-		status "close": "catchall: 0104 0000 01 01 0040 00 0A21 00 00 0000 0A 00 0A6F6666"
-
-		// reply messages
-		reply "raw 0x0 { 00 00 0a 0a 6f 6e }": "catchall: 0104 0000 01 01 0040 00 0A21 00 00 0000 0A 00 0A6F6E"
-		reply "raw 0x0 { 00 00 0a 0a 6f 66 66 }": "catchall: 0104 0000 01 01 0040 00 0A21 00 00 0000 0A 00 0A6F6666"
 	}
 
 	// UI tile definitions
 	tiles {
-		standardTile("windowShade", "device.windowShade", width: 2, height: 2, canChangeIcon: true, canChangeBackground: true) {
-//			state "on", label: '${name}', action: "switch.off", icon: "st.doors.garage.garage-open", backgroundColor: "#79b821"
-//			state "off", label: '${name}', action: "switch.on", icon: "st.doors.garage.garage-closed", backgroundColor: "#ffffff"
-			state "open", label: '${name}', action: "switch.off", icon: "st.doors.garage.garage-open", backgroundColor: "#79b821"
-			state "opening", label: '${name}', icon: "st.doors.garage.garage-opening", backgroundColor: "#b9b821"
-			state "close", label: '${name}', action: "switch.on", icon: "st.doors.garage.garage-closed", backgroundColor: "#ffffff"
-			state "closing", label: '${name}', icon: "st.doors.garage.garage-closing", backgroundColor: "#b9b821"
+		standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true, canChangeBackground: true) {
+			// state "on", label: '${name}', action: "switch.off", icon: "st.switches.switch.on", backgroundColor: "#79b821"
+			// state "off", label: '${name}', action: "switch.on", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
+			state "on", label: 'opened', action: "switch.off", icon: "st.doors.garage.garage-open", backgroundColor: "#79b821"
+			state "off", label: 'closed', action: "switch.on", icon: "st.doors.garage.garage-closed", backgroundColor: "#ffffff"
+            state "opening", label: '${name}', icon: "st.doors.garage.garage-opening", backgroundColor: "#b9b821"
+            state "closing", label: '${name}', icon: "st.doors.garage.garage-closing", backgroundColor: "#b9b821"
 		}
-        
-		main "windowShade"
-		details "windowShade"
+
+		main "switch"
+		details "switch"
 	}
 }
 
 // Parse incoming device messages to generate events
 def parse(String description) {
 	def value = zigbee.parse(description)?.text
-	def name = value in ["on","off","closing","opening","open","close","status"] ? "windowShade" : null
-    log.debug "Window shade data received ${name} : ${value}"
+	def name = value in ["on","off","opening","closing"] ? "switch" : null
 	def result = createEvent(name: name, value: value)
-	log.debug "Window shade parse returned ${result?.descriptionText}"
+	log.debug "Parse returned ${result?.descriptionText}"
 	return result
 }
 
 // Commands sent to the device
 def on() {
-	log.debug("sent open command to device");
-	zigbee.smartShield(text: "open").format()
+	zigbee.smartShield(text: "on").format()
 }
 
 def off() {
-	log.debug("sent close command to device");
-	zigbee.smartShield(text: "close").format()
+	zigbee.smartShield(text: "off").format()
 }
 
-def status() {
-	log.debug("sent status request to device")
-    zigbee.smartShield(text: "status").format()
-}
